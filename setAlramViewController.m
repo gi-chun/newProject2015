b9e6ed0d5d6487fef18f89499c3ac66bb748431a
gclee <springgclee@gmail.com>
2015-12-17 21:08:00 +0900

    set alram get, set

--- setAlramViewController.m
+++ setAlramViewController.m
@@ -8,6 +8,8 @@
 
 #import "setAlramViewController.h"
 #import "NavigationBarView.h"
+#import "AFHTTPRequestOperationManager.h"
+#import "SBJson.h"
 
 @interface setAlramViewController ()
 {
@@ -123,6 +125,8 @@
             marginX = kPopWindowMarginW;
         }
     }
+    
+    [self getAlarmInfo];
     
     NSString* temp;
     temp = [[NSUserDefaults standardUserDefaults] stringForKey:klang];
@@ -253,8 +257,321 @@
     return navigationBarView;
 }
 
+- (void)getAlarmInfo
+{
+    UIImageView *likeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 112, 112)];
+    [likeImageView setCenter:CGPointMake(kScreenBoundsWidth/2, kScreenBoundsHeight/2)];
+    [likeImageView setImage:[UIImage imageNamed:@"loding_cha_01@3x.png"]];
+    [self.view addSubview:likeImageView];
+    [self.view bringSubviewToFront:likeImageView];
+    
+    if ([SYSTEM_VERSION intValue] > 7) {
+        likeImageView.transform = CGAffineTransformMakeScale(0.1, 0.1);
+        [UIView animateWithDuration:3.0f
+                              delay:0
+             usingSpringWithDamping:0.2f
+              initialSpringVelocity:6.0f
+                            options:UIViewAnimationOptionAllowUserInteraction
+                         animations:^{
+                             likeImageView.transform = CGAffineTransformIdentity;
+                         }
+                         completion:^(BOOL finished) {
+                             [likeImageView removeFromSuperview];
+                         }];
+    }
+    else {
+        [UIView animateWithDuration:1.0f animations:^{
+            [likeImageView removeFromSuperview];
+        }];
+    }
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSMutableDictionary *sendDic = [NSMutableDictionary dictionary];
+    NSMutableDictionary *rootDic = [NSMutableDictionary dictionary];
+    NSMutableDictionary *indiv_infoDic = [NSMutableDictionary dictionary];
+    
+    //
+    [rootDic setObject:@"sfg.sunny.task.user.AlarmTask" forKey:@"task"];
+    [rootDic setObject:@"getAlarm" forKey:@"action"];
+    [rootDic setObject:@"" forKey:@"serviceCode"];
+    [rootDic setObject:@"" forKey:@"requestMessage"];
+    [rootDic setObject:@"" forKey:@"responseMessage"];
+    
+    if([[NSUserDefaults standardUserDefaults] stringForKey:kCardCode]){
+        [indiv_infoDic setObject:[[NSUserDefaults standardUserDefaults] stringForKey:kCardCode] forKey:@"user_seq"];
+    }
+    
+    [sendDic setObject:rootDic forKey:@"root_info"];
+    [sendDic setObject:indiv_infoDic forKey:@"indiv_info"];//////
+    
+    SBJsonWriter *jsonWriter = [[SBJsonWriter alloc] init];
+    NSString *jsonString = [jsonWriter stringWithObject:sendDic];
+    NSLog(@"request json: %@", jsonString);
+    
+    NSDictionary *parameters = @{@"plainJSON": jsonString};
+    
+    [manager POST:API_URL parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    
+        NSLog(@"JSON: %@", responseObject);
+        
+        NSString *responseData = (NSString*) responseObject;
+        NSArray *jsonArray = (NSArray *)responseData;
+        NSDictionary * dicResponse = (NSDictionary *)responseData;
+        NSLog(@"Response ==> %@", responseData);
+        
+        //warning
+        NSDictionary *dicItems = [dicResponse objectForKey:@"WARNING"];
+        
+        if(dicItems){
+            NSString* sError = dicItems[@"msg"];
+            
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:sError delegate:nil cancelButtonTitle:@"close" otherButtonTitles:nil, nil];
+            [alert show];
+            [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kLoginY];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            
+        }else{
+            
+            dicItems = nil;
+            dicItems = [dicResponse objectForKey:@"indiv_info"];
+            NSString* sPushY = dicItems[@"push_rec_yn"];
+            
+            if([sPushY isEqualToString:@"Y"]){
+                
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+
+            }else{
+                
+                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+            }
+            
+            NSString* sTotalSet = dicItems[@"push_alarm"];
+            
+            NSString *nomalSet;
+            NSString *contentSet;
+            NSString *eventSet;
+            
+            //1|Y,2|Y,3|Y
+            NSRange range = {2,1};
+            nomalSet = [sTotalSet substringWithRange:range];
+            NSRange range_ = {6,1};
+            contentSet = [sTotalSet substringWithRange:range_];
+            NSRange range__ = {10,1};
+            eventSet = [sTotalSet substringWithRange:range__];
+            
+            if([nomalSet isEqualToString:@"Y"]){
+                
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kNomalPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+                
+            }else{
+                
+                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kNomalPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+            }
+
+            if([contentSet isEqualToString:@"Y"]){
+                
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kContentsPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+                
+            }else{
+                
+                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kContentsPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+            }
+
+            if([eventSet isEqualToString:@"Y"]){
+                
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kEventPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+                
+            }else{
+                
+                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kEventPushY];
+                [[NSUserDefaults standardUserDefaults] synchronize];
+            }
+            
+            NSLog(@"Response ==> %@", responseData);
+            
+            //to json
+            SBJsonWriter *jsonWriter = [[SBJsonWriter alloc] init];
+            NSString *jsonString = [jsonWriter stringWithObject:jsonArray];
+            NSLog(@"jsonString ==> %@", jsonString);
+            //save login info
+            [[NSUserDefaults standardUserDefaults] setObject:jsonString forKey:kLoginData];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookieAcceptPolicy:NSHTTPCookieAcceptPolicyAlways];
+            NSHTTPCookie *cookie;
+            NSMutableDictionary *cookieProperties = [NSMutableDictionary dictionary];
+            [cookieProperties setObject:@"locale_" forKey:NSHTTPCookieName];
+            //        [cookieProperties setObject:[NSString stringWithFormat:@"%f", [[NSDate date] timeIntervalSince1970]] forKey:NSHTTPCookieValue];
+            //////////////////////////////////////
+            [cookieProperties setObject:@"KO" forKey:NSHTTPCookieValue];
+            [cookieProperties setObject:@"vntst.shinhanglobal.com" forKey:NSHTTPCookieDomain];
+            [cookieProperties setObject:@"vntst.shinhanglobal.com" forKey:NSHTTPCookieOriginURL];
+            [cookieProperties setObject:@"/" forKey:NSHTTPCookiePath];
+            [cookieProperties setObject:@"0" forKey:NSHTTPCookieVersion];
+            // set expiration to one month from now
+            [cookieProperties setObject:[[NSDate date] dateByAddingTimeInterval:2629743] forKey:NSHTTPCookieExpires];
+            cookie = [NSHTTPCookie cookieWithProperties:cookieProperties];
+            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+            
+            for (cookie in [NSHTTPCookieStorage sharedHTTPCookieStorage].cookies) {
+                NSLog(@"%@=%@", cookie.name, cookie.value);
+            }
+        }
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        NSLog(@"Error: %@", error);
+        
+    }];
+}
+
 - (void)didTouchBackButton
 {
+    UIImageView *likeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 112, 112)];
+    [likeImageView setCenter:CGPointMake(kScreenBoundsWidth/2, kScreenBoundsHeight/2)];
+    [likeImageView setImage:[UIImage imageNamed:@"loding_cha_01@3x.png"]];
+    [self.view addSubview:likeImageView];
+    [self.view bringSubviewToFront:likeImageView];
+    
+    if ([SYSTEM_VERSION intValue] > 7) {
+        likeImageView.transform = CGAffineTransformMakeScale(0.1, 0.1);
+        [UIView animateWithDuration:3.0f
+                              delay:0
+             usingSpringWithDamping:0.2f
+              initialSpringVelocity:6.0f
+                            options:UIViewAnimationOptionAllowUserInteraction
+                         animations:^{
+                             likeImageView.transform = CGAffineTransformIdentity;
+                         }
+                         completion:^(BOOL finished) {
+                             [likeImageView removeFromSuperview];
+                         }];
+    }
+    else {
+        [UIView animateWithDuration:1.0f animations:^{
+            [likeImageView removeFromSuperview];
+        }];
+    }
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    NSMutableDictionary *sendDic = [NSMutableDictionary dictionary];
+    NSMutableDictionary *rootDic = [NSMutableDictionary dictionary];
+    NSMutableDictionary *indiv_infoDic = [NSMutableDictionary dictionary];
+    
+    NSString *nomalSet;
+    NSString *contentSet;
+    NSString *eventSet;
+    NSString *totalSet;
+    
+    BOOL isAlram = [[NSUserDefaults standardUserDefaults] boolForKey:kPushY];
+    if(isAlram == YES){
+        
+        BOOL isAlram = [[NSUserDefaults standardUserDefaults] boolForKey:kNomalPushY];
+        if(isAlram == YES){
+            nomalSet = @"Y";
+        }else{
+            nomalSet = @"N";
+        }
+        
+        isAlram = [[NSUserDefaults standardUserDefaults] boolForKey:kContentsPushY];
+        if(isAlram == YES){
+            contentSet = @"Y";
+        }else{
+            contentSet = @"N";
+        }
+        
+        isAlram = [[NSUserDefaults standardUserDefaults] boolForKey:kEventPushY];
+        if(isAlram == YES){
+            eventSet = @"Y";
+        }else{
+            eventSet = @"N";
+        }
+        
+    }else{
+        nomalSet = @"N";
+        contentSet = @"N";
+        eventSet = @"N";
+    }
+    
+    totalSet = [NSString stringWithFormat:@"1|%@,2|%@,3|%@", nomalSet, contentSet, eventSet];
+    //
+    [rootDic setObject:@"sfg.sunny.task.user.AlarmTask" forKey:@"task"];
+    [rootDic setObject:@"setAlarm" forKey:@"action"];
+    [rootDic setObject:@"" forKey:@"serviceCode"];
+    [rootDic setObject:@"" forKey:@"requestMessage"];
+    [rootDic setObject:@"" forKey:@"responseMessage"];
+    
+    if([[NSUserDefaults standardUserDefaults] stringForKey:kCardCode]){
+        [indiv_infoDic setObject:[[NSUserDefaults standardUserDefaults] stringForKey:kCardCode] forKey:@"user_seq"];
+    }
+    [indiv_infoDic setObject:totalSet forKey:@"push_alarm"];
+    
+    [sendDic setObject:rootDic forKey:@"root_info"];
+    [sendDic setObject:indiv_infoDic forKey:@"indiv_info"];//////
+    
+    SBJsonWriter *jsonWriter = [[SBJsonWriter alloc] init];
+    NSString *jsonString = [jsonWriter stringWithObject:sendDic];
+    NSLog(@"request json: %@", jsonString);
+    
+    NSDictionary *parameters = @{@"plainJSON": jsonString};
+    
+    [manager POST:API_URL parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        
+        NSLog(@"JSON: %@", responseObject);
+        
+        NSString *responseData = (NSString*) responseObject;
+        NSArray *jsonArray = (NSArray *)responseData;
+        NSDictionary * dicResponse = (NSDictionary *)responseData;
+        NSLog(@"Response ==> %@", responseData);
+        
+        //warning
+        NSDictionary *dicItems = [dicResponse objectForKey:@"WARNING"];
+        
+        if(dicItems){
+            NSString* sError = dicItems[@"msg"];
+            
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil message:sError delegate:nil cancelButtonTitle:@"close" otherButtonTitles:nil, nil];
+            [alert show];
+            [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kLoginY];
+            [[NSUserDefaults standardUserDefaults] synchronize];
+            
+        }else{
+            
+            //to json
+            SBJsonWriter *jsonWriter = [[SBJsonWriter alloc] init];
+            
+            NSString *jsonString = [jsonWriter stringWithObject:jsonArray];
+            NSLog(@"jsonString ==> %@", jsonString);
+            ///////////////////////////////
+            
+            for (NSHTTPCookie *cookie in [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies])
+            {
+                NSLog(@"name: '%@'\n",   [cookie name]);
+                NSLog(@"value: '%@'\n",  [cookie value]);
+                NSLog(@"domain: '%@'\n", [cookie domain]);
+                NSLog(@"path: '%@'\n",   [cookie path]);
+            }
+            
+            NSLog(@"getCookie end ==>" );
+            
+        }
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        NSLog(@"Error: %@", error);
+        
+    }];
+    
+    
     [self.navigationController popViewControllerAnimated:YES];
     
     //    if ([self.delegate respondsToSelector:@selector(didTouchBackButton)]) {
